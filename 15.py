98d699f0ec0e088822dfb9455b8f3e04008c5060
Cássio Filipe Meireles De Carvalho Braga <78364193+Cassio06@users.noreply.github.com>
2024-08-18 07:33:02 -0300

    sim
    sim2

---
+++ 15.py
@@ -0,0 +1,4 @@
+import math
+num = float(input('Digite um numero com no minimo uma casa decimal: '))
+num_int = math.trunc(num)
+print(f'O numero escolhido foi {num} e sua versão inteira é {num_int}')
---
+++ 16.py
@@ -0,0 +1,4 @@
+catetoad = int(input('Diga um numero: '))
+catetoop = int(input('Diga outro numero: '))
+hi = (catetoad ** 2 + catetoop ** 2)**(1/2)
+print(f'O cateto adjascente é {catetoad}, o cateto oposto é {catetoop} já a hipotenusa é {hi} ')
